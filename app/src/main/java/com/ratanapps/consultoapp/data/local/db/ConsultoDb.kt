ff1de17108be6cf5f5ff7ff66815cc2e4e97ce83
shivamratan <shivamratan61@gmail.com>
2026-04-28 19:35:47 +0530

    Boilerplate Code: Theme + loading basic code addition

--- app/src/main/java/com/ratanapps/consultoapp/data/local/db/ConsultoDb.kt
+++ app/src/main/java/com/ratanapps/consultoapp/data/local/db/ConsultoDb.kt
@@ -2,8 +2,9 @@ package com.ratanapps.consultoapp.data.local.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.ratanapps.consultoapp.data.local.entity.UserEntity
 
-@Database(entities = [], version = 1, exportSchema = false)
+@Database(entities = [UserEntity::class], version = 1, exportSchema = false)
 abstract class ConsultoDb : RoomDatabase() {
     // DAOs will be added here
 }
--- app/src/main/java/com/ratanapps/consultoapp/ui/HomeViewModel.kt
+++ app/src/main/java/com/ratanapps/consultoapp/ui/HomeViewModel.kt
@@ -8,11 +8,12 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 
 // Global Activity ViewModel
 @HiltViewModel
-class HomeViewModel: ViewModel() {
+class HomeViewModel @Inject constructor(): ViewModel() {
 
     private val _isLoading = MutableStateFlow(true)
     val isLoading: StateFlow<Boolean> = _isLoading
