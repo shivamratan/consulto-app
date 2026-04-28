8498d907a1c8d68bc69ee0d7237c01151f50e314
shivamratan <shivamratan61@gmail.com>
2026-04-28 12:25:01 +0530

    Size update

--- app/src/main/java/com/ratanapps/consultoapp/MainActivity.kt
+++ app/src/main/java/com/ratanapps/consultoapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ratanapps.consultoapp.ui.theme.ConsultoAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,7 +35,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier.padding(5.dp)
     )
 }
 
